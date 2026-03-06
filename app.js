a23f08b54068f0ae6dd45a6d23541b069146ea9c
Steven-lei <leizhengwen@126.com>
2026-03-07 12:52:00 +1300

    simplest website

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
+var express = require("express");
+var app = express();
+
 var monent = require("moment");
 var date = monent().format("YYYY-MM-DD, h:mm:ss a");
 console.log("today is ", date);
+
+app.get("/", function (req, res) {
+  res.send("Hello World!");
+});
+
+app.listen(3000, function () {
+  console.log("Example app listening on port 3000!");
+});
