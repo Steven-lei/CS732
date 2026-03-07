8ede9b04e0ba5c7a24d540c36c10acab6f29eebe
Steven-lei <leizhengwen@126.com>
2026-03-07 13:09:15 +1300

    add dockerfile

--- app.js
+++ app.js
@@ -2,11 +2,10 @@ var express = require("express");
 var app = express();
 
 var monent = require("moment");
-var date = monent().format("YYYY-MM-DD, h:mm:ss a");
-console.log("today is ", date);
 
 app.get("/", function (req, res) {
-  res.send("Hello World!");
+  var date = monent().format("YYYY-MM-DD, h:mm:ss a");
+  res.send("Today is " + date);
 });
 
 app.listen(3000, function () {
